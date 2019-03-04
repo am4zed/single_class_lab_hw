bc3c3b74826fd1d6a1ce884fb474642063eee4cb
Amy Edwardson <am4zed11@gmail.com>
2019-03-04 22:42:45 +0000

    Some further attempts at the extension.

--- Extension.rb
+++ Extension.rb
@@ -18,16 +18,24 @@ class Library
   #   return@books.find{|book_hash| book_hash[:title] == title}
   # end
 
-#   def get_rental_details_by_title(title)
-#     for book_hash in @books
-#       if book_hash[:title] == title
-#         return book_hash[:rental_details]
-#       end
-#     end
-#   end
+  def get_rental_details_by_title(title)
+    for book_hash in @books
+      if book_hash[:title] == title
+        return book_hash[:rental_details]
+      end
+    end
+  end
 #
   def add_book_by_title(title)
     @books.push({title: title, rental_details: {student_name: "", date: ""}})
   end
-#
-# end
+
+  def change_rental_details(title, student, due_date)
+    for book_hash in @books
+    if book_hash[:title] == title
+      book_hash[:student_name] = student
+      book_hash[:due_date] = due_date
+    end
+  end
+
+end
--- specs/Extension_spec.rb
+++ specs/Extension_spec.rb
@@ -45,20 +45,31 @@ class TestLibrary < MiniTest::Test
       }, book)
   end
 
-# def test_get_rental_details_by_title
-#   library = Library.new(@books)
-#   book = library.get_rental_details_by_title("Catcher_in_the_rye")
-#   assert_equal("{
-#     student_name: "John Jonsen",
-#     date: "05/05/18"
-#     }", book )
-# end
-#
-def add_book_by_title
+def test_get_rental_details_by_title
+  library = Library.new(@books)
+  book = library.get_rental_details_by_title("Catcher_in_the_rye")
+  assert_equal("{
+    student_name: "John Jonsen",
+    date: "05/05/18"
+    }", book )
+end
+
+def test_add_book_by_title
   library = Library.new(@books)
   new_book = library.add_book_by_title("Birdsong")
   assert_equal(true, new_book.include?)
 end
+
+def test_change_rental_details
+  library = Library.new(@books)
+  changed_rental_details = library.change_rental_details("Dune", "Mary Berry", "27/05/18")
+  assert_equal({
+    student_name: "Mary Berry",
+    date: "27/05/18"
+  }, changed_rental_details)
+end
+
+
 
 
 end
